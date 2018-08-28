24308ddea0a13a0e440061bae6b1e58188460214
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-28 15:48:42 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m2/Assignment-3/Solution.java
+++ assignments/m2/Assignment-3/Solution.java
@@ -1,39 +1,42 @@
 import java.util.Scanner;
+/**
+ * { item_description }.
+ */
 
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		/**
-		 * { item_description }.
-		 */
-	}
-	/**
-	 * {This is the function where we write take the inputs}.
-	 *
-	 * @param      args  type is string
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		int base = s.nextInt();
-		int exponent = s.nextInt();
-		long result = power(base, exponent);
-		System.out.println(result);
-	}
-	/**
-	 * {recursion function for power}.
-	 *
-	 * @param      b     {type int }.
-	 * @param      e     {type int}.
-	 *
-	 * @return     { returns the power }.
-	 */
-	public static long power(final int b, final int e) {
-		if (e != 0) {
-			return (b * power(b, e - 1));
-		} else {
-			return 1;
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * { item_description }.
+         */
+    }
+    /**
+     * {This is the function where we write take the inputs}.
+     *
+     * @param      args  type is string
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        long result = power(base, exponent);
+        System.out.println(result);
+    }
+    /**
+     * {recursion function for power}.
+     *
+     * @param      b     {type int }.
+     * @param      e     {type int}.
+     *
+     * @return     { returns the power }.
+     */
+    public static long power(final int b, final int e) {
+        if (e != 0) {
+            return (b * power(b, e - 1));
+        } else {
+            return 1;
+        }
+    }
 }
