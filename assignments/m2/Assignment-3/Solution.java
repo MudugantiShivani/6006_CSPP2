1a459f746a5e82982bf91c8011ef575a0c6dbd98
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-28 15:45:46 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m2/Assignment-3/Solution.java
+++ assignments/m2/Assignment-3/Solution.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
 
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
+public final class Solution {
+	/**
+	 * {This is the function where we write take the inputs}.
+	 *
+	 * @param      args  type is string
+	 */
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		int base = s.nextInt();
 		int exponent = s.nextInt();
 		long result = power(base, exponent);
 		System.out.println(result);
 	}
-	public static long power(int b, int e) {
+	/**
+	 * {recursion function for power}
+	 *
+	 * @param      b     {type int }.
+	 * @param      e     {type int}.
+	 *
+	 * @return     { returns the power }.
+	 */
+	public static long power(final int b, final int e) {
 		if (e != 0) {
 			return (b * power(b, e - 1));
 		} else {
