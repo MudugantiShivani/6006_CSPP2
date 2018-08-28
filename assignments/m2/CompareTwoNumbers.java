083cc3cd201c78a5d3729c649b7df4e8903be6c6
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-28 17:26:22 +0530

    comparing

---
+++ assignments/m2/CompareTwoNumbers.java
@@ -0,0 +1,19 @@
+public class CompareTwoNumbers {
+ 
+	public static void main(String[] args) {
+		
+		
+		int VarA = 324;
+		int VarB = 234;
+		
+		if(VarA > VarB){
+			System.out.println("Bigger");
+		}
+		else if(VarA < VarB){
+			System.out.println("Smaller");
+		}
+		else{
+			System.out.println("Equal");
+		}
+	}
+}
