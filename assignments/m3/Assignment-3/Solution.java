884680e908a45f8802c4c08102706a458e882dbf
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-29 12:01:39 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m3/Assignment-3/Solution.java
+++ assignments/m3/Assignment-3/Solution.java
@@ -1,18 +1,38 @@
 
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
+/**
+ * Class for solution.
+ */
 public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * { this is a constructor}.
+         */
+    }
+    /**
+     * {this is the main function where we declare values}.
+     *
+     * @param      args  String type
+     */
 
     public static void main(String[] args) {
-
         Scanner s=new Scanner(System.in);      
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        int gcd=gcd(n1,n2);
+        int gcd = gcd(n1,n2);
         System.out.println(gcd);
     }
+    /**
+     * { function to find GCD of two numbers}.
+     *
+     * @param      i     { int data type }
+     * @param      j     { int data type }
+     *
+     * @return     { value of the function is returned }
+     */
     public static int gcd(int i, int j){
         if (j!=0){
             return gcd(j, i%j);
