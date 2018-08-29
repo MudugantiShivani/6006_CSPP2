8c05e9de62738f80d53078c17fcec6a9dca8edde
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-29 13:53:25 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m3/Assignment-2/Solution.java
+++ assignments/m3/Assignment-2/Solution.java
@@ -1,31 +1,39 @@
 
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
+/**
+ * Class for solution.
+ */
 public class Solution {
-/* Fill the main function to print the number of 7's between 1 to n*/
-    public static void main(String[] args) {
+	/**
+	 * {function to print number of 7's from 1 to given number}.
+	 *
+	 * @param      args  string data type
+	 */
+	public static void main(String[] args) {
 
-        Scanner s=new Scanner(System.in);      
-        int n = s.nextInt();
-        int seven = seven(n);
-        System.out.println(seven); 
-    }
-public static int seven(int n)
-{
-    int i = 0, j = 1;
-    int count = 0;
-    for (i = 1; i <= n; i ++)
-    {
-        j = i;
-        while (j != 0)
-        {
-            if (j % 10 == 7)
-                count ++;
-            j /= 10; 
-        }
-    }
-    return count;
-}
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int seven = seven(n);
+		System.out.println(seven);
+	}
+	/**
+	 * {Here the function counts number of 7's}.
+	 *
+	 * @param      n     { int data type and reads the number}
+	 *
+	 * @return     { returns the count of number of 7's }
+	 */
+	public static int seven(int n) {
+		int i = 0, j = 1;
+		int count = 0;
+		for (i = 1; i <= n; i ++) {
+			j = i;
+			while (j != 0) {
+				if (j % 10 == 7)
+					count ++;
+				j /= 10;
+			}
+		}
+		return count;
+	}
 }
