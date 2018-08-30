42e392cffd29b59fd6d2c050a5df17b4493e500e
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-30 14:13:42 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m4/Assignment-1/Solution.java
+++ assignments/m4/Assignment-1/Solution.java
@@ -20,7 +20,7 @@ public final class Solution {
         int n, largestnumber;
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        int arr[] = new int[n];
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
