c1fef1d7ce821153c73511dfbbcb5791ec4de304
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-30 14:56:51 +0530

    "check_style_score -> 2 of 4 passed. style score: 1/1 "

--- assignments/m4/Assignment-2/Solution.java
+++ assignments/m4/Assignment-2/Solution.java
@@ -3,58 +3,58 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		/**
-		 * {This is the private calss}
-		 */
-	}
-	/**
-	 * { addition of matrices function }.
-	 *
-	 * @param      args data type is string
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		int row1 = s.nextInt();
-		int column1 = s.nextInt();
-		int[][] arr1 = new int[row1][column1];
-		for (int i = 0; i < row1; i++) {
-			for (int j = 0; j < column1; j++) {
-				arr1[i][j] = s.nextInt();
-			}
-		}
-		int row2 = s.nextInt();
-		int column2 = s.nextInt();
-		int[][] arr2 = new int[row2][column2];
-		for (int i = 0; i < row2; i++) {
-			for (int j = 0; j < column2; j++) {
-				arr2[i][j] = s.nextInt();
-			}
-		}
-		if (row1 == row2 && column1 == column2) {
-			int[][] sum = new int[row1][column1];
-			for (int i = 0; i < row2; i++) {
-				for (int j = 0; j < column2; j++) {
-					sum[i][j] = arr1[i][j] + arr2[i][j];
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * {This is the private calss}
+         */
+    }
+    /**
+     * { addition of matrices function }.
+     *
+     * @param      args data type is string
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int row1 = s.nextInt();
+        int column1 = s.nextInt();
+        int[][] arr1 = new int[row1][column1];
+        for (int i = 0; i < row1; i++) {
+            for (int j = 0; j < column1; j++) {
+                arr1[i][j] = s.nextInt();
+            }
+        }
+        int row2 = s.nextInt();
+        int column2 = s.nextInt();
+        int[][] arr2 = new int[row2][column2];
+        for (int i = 0; i < row2; i++) {
+            for (int j = 0; j < column2; j++) {
+                arr2[i][j] = s.nextInt();
+            }
+        }
+        if (row1 == row2 && column1 == column2) {
+            int[][] sum = new int[row1][column1];
+            for (int i = 0; i < row2; i++) {
+                for (int j = 0; j < column2; j++) {
+                    sum[i][j] = arr1[i][j] + arr2[i][j];
 
-				}
-			}
-			for (int i = 0; i < row2; i++) {
-				for (int j = 0; j < column2; j++) {
-					System.out.print(sum[i][j] + " ");
-				}
-				System.out.println();
-			}
+                }
+            }
+            for (int i = 0; i < row2; i++) {
+                for (int j = 0; j < column2; j++) {
+                    System.out.print(sum[i][j] + " ");
+                }
+                System.out.println();
+            }
 
-		} else {
-			System.out.println("not possible");
-		}
-
-
+        } else {
+            System.out.println("not possible");
+        }
 
 
-	}
+
+
+    }
 }
