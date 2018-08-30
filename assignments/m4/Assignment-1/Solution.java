9ceafe855100baa66edbf513d2dedc9758418e75
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-30 14:10:05 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m4/Assignment-1/Solution.java
+++ assignments/m4/Assignment-1/Solution.java
@@ -1,22 +1,20 @@
 import java.util.*;
-public class Solution
-{
-	public static void main(String[] args)
-	{
-		int n,largestnumber;
-		Scanner sc=new Scanner(System.in);
-		n= sc.nextInt();
-		int arr[] = new int[n];
-		for(int i = 0; i < n; i++){
-			arr[i] = sc.nextInt();
-		}
-		largestnumber = arr[0];
-		for (int i=0;i<n;i++){
-			if (largestnumber<arr[i]){
-				largestnumber = arr[i];
-			}
-		}
-		System.out.println(largestnumber);
+public class Solution {
+    public static void main(String[] args) {
+        int n, largestnumber;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        largestnumber = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (largestnumber < arr[i]) {
+                largestnumber = arr[i];
+            }
+        }
+        System.out.println(largestnumber);
 
-		}
+    }
 }
