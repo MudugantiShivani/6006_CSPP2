a14541d57be8b0de7d33504b194cc4f601ca56a4
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-30 15:24:25 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- assignments/m4/Assignment-2/Solution.java
+++ assignments/m4/Assignment-2/Solution.java
@@ -20,25 +20,25 @@ public final class Solution {
         Scanner s = new Scanner(System.in);
         int row1 = s.nextInt();
         int column1 = s.nextInt();
-        int[][] arr1 = new int[row1][column1];
+        int[][] array1 = new int[row1][column1];
         for (int i = 0; i < row1; i++) {
             for (int j = 0; j < column1; j++) {
-                arr1[i][j] = s.nextInt();
+                array1[i][j] = s.nextInt();
             }
         }
         int row2 = s.nextInt();
         int column2 = s.nextInt();
-        int[][] arr2 = new int[row2][column2];
+        int[][] array2 = new int[row2][column2];
         for (int i = 0; i < row2; i++) {
             for (int j = 0; j < column2; j++) {
-                arr2[i][j] = s.nextInt();
+                array2[i][j] = s.nextInt();
             }
         }
         if (row1 == row2 && column1 == column2) {
             int[][] sum = new int[row1][column1];
             for (int i = 0; i < row2; i++) {
                 for (int j = 0; j < column2; j++) {
-                    sum[i][j] = arr1[i][j] + arr2[i][j];
+                    sum[i][j] = array1[i][j] + array2[i][j];
 
                 }
             }
