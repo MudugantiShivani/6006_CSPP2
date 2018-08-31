3361fe933ab69f9fc28fd882b1f22ab0aa5e6189
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-31 17:23:07 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- assignments/m4/Assignment-3/Solution.java
+++ assignments/m4/Assignment-3/Solution.java
@@ -1,35 +1,26 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		sc.nextLine();
-		for(int i=0;i<=n;i++){
-			String s=sc.nextLine();
-			long res=binaryToDecimal(s);
+public class Solution {
+	static long binaryToDecimal(final String s) {
+		long binarynum = (long) Long.parseLong(s);
+		int b = 0;
+		final int ten = 10;
+		long result = 0;
+		long num = binarynum;
+		while (num > 0) {
+			long x = num % ten;
+			result += x * Math.pow(2, b);
+			num = num / ten;
+			b = b + 1;
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();sc.nextLine();
+		for (int i = 0; i <= n; i++) {
+			String s = sc.nextLine();
+			long res = binaryToDecimal(s);
 			System.out.println(res);
 		}
 	}
-	static long binaryToDecimal(final String s){
-		long i = (long) Long.parseLong(s);
-        int b = 0;
-        final int ten = 10;
-        long sum = 0;
-        long y = i;
-        while (y > 0) {
-            long x = y % ten;
-            sum += x * Math.pow(2, b);
-            y = y / ten;
-            b = b + 1;
-        }
-        return sum;
-
-	}
-
 }
