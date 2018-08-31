7ef7d3b4b8907fe1cb5268cca53a4fd6e5a6512f
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-31 14:59:45 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m4/Assignment-4/Solution.java
+++ assignments/m4/Assignment-4/Solution.java
@@ -1,19 +1,41 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverseerse = reverseerseString(s);
-		System.out.println(reverseerse);
-
-	}
-	static String reverseerseString(final String reverse) {
-		String result = "";
-		char[] array1 = reverse.toCharArray();
-		int len = reverse.length();
-		for (int k = len - 1; k >= 0; k--) {
-			result += array1[k];
-		}
-		return result;
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * {private class also called constructor}.
+         */
+    }
+    /**
+     * {main function which prints the reverse of a string}.
+     *
+     * @param      args string data type
+     */
+    public static void main( final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverseerse = reverseerseString(s);
+        System.out.println(reverseerse);
+    }
+    /**
+     * {This is the function where we write code for reverse of string}.
+     *
+     * @param      reverse data type is int and reverse of the string is stored
+     *
+     * @return     { returns the reversed string }
+     */
+    static String reverseerseString(final String reverse) {
+        String result = "";
+        char[] array1 = reverse.toCharArray();
+        int len = reverse.length();
+        for (int k = len - 1; k >= 0; k--) {
+            result += array1[k];
+        }
+        return result;
+    }
 }
