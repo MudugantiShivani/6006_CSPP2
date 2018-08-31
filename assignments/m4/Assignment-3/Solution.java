fff7084bdfe63bd53f5de49652cc48a831386225
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-31 17:24:50 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- assignments/m4/Assignment-3/Solution.java
+++ assignments/m4/Assignment-3/Solution.java
@@ -16,8 +16,7 @@ public class Solution {
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.nextLine();
+		int n = sc.nextInt();sc.nextLine();
 		for (int i = 0; i <= n; i++) {
 			String s = sc.nextLine();
 			long res = binaryToDecimal(s);
