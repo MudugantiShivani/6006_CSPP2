0d7fb676f509e378dc513be302d679e062d839d9
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-31 17:31:13 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- assignments/m4/Assignment-3/Solution.java
+++ assignments/m4/Assignment-3/Solution.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
-public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.nextLine();
-		for (int i = 0; i <= n; i++) {
-			String s = sc.nextLine();
-			long res = binaryToDecimal(s);
-			System.out.println(res);
-		}
-	}
-	static long binaryToDecimal(final String s) {
-		long i = (long) Long.parseLong(s);
-		int b = 0;
-		final int ten = 10;
-		long sum = 0;
-		long y = i;
-		while (y > 0) {
-			long x = y % ten;
-			sum += x * Math.pow(2, b);
-			y = y / ten;
-			b = b + 1;
-		}
-		return sum;
-
-	}
+public final class Solution {
+    static long binaryToDecimal(final String s) {
+        long i = (long) Long.parseLong(s);
+        int b = 0;
+        final int ten = 10;
+        long sum = 0;
+        long y = i;
+        while (y > 0) {
+            long x = y % ten;
+            sum += x * Math.pow(2, b);
+            y = y / ten;
+            b = b + 1;
+        }
+        return sum;
+    }
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            long res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
 
 }
