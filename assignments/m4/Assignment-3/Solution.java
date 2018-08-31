55d9a09f9062e16958f454690f803d402553c6ca
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-31 17:36:27 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- assignments/m4/Assignment-3/Solution.java
+++ assignments/m4/Assignment-3/Solution.java
@@ -1,19 +1,39 @@
 import java.util.Scanner;
+/**
+ * { program to convert binary to decimal}.
+ */
 public final class Solution {
+    private Solution() {
+        /**
+         * {private constructor}.
+         */
+    } 
+    /**
+     * { function_description }.
+     *
+     * @param      s     { string data type }
+     *
+     * @return     {returns the decimal number}
+     */
     static long binaryToDecimal(final String s) {
         long binarynum = Long.parseLong(s);
         int b = 0;
         final int ten = 10;
-        long sum = 0;
+        long result = 0;
         long num = binarynum;
         while (num > 0) {
             long num1 = num % ten;
-            sum += num1 * Math.pow(2, b);
+            result += num1 * Math.pow(2, b);
             num = num / ten;
             b = b + 1;
         }
-        return sum;
+        return result;
     }
+    /**
+     * { main function }.
+     *
+     * @param      args  string data type
+     */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
