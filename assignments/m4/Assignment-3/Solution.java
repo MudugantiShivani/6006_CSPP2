fd90f0c6a0c96fc719f494bae5aceec08e7bf2f6
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-31 17:33:19 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- assignments/m4/Assignment-3/Solution.java
+++ assignments/m4/Assignment-3/Solution.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public final class Solution {
     static long binaryToDecimal(final String s) {
-        long i = Long.parseLong(s);
+        long binarynum = Long.parseLong(s);
         int b = 0;
         final int ten = 10;
         long sum = 0;
-        long y = i;
-        while (y > 0) {
-            long x = y % ten;
-            sum += x * Math.pow(2, b);
-            y = y / ten;
+        long num = binarynum;
+        while (num > 0) {
+            long num1 = num % ten;
+            sum += num1 * Math.pow(2, b);
+            num = num / ten;
             b = b + 1;
         }
         return sum;
