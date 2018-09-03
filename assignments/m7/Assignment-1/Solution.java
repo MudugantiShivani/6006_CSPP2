25f24746ac8bead5ef381ca22573fd278f9e59c6
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-09-03 18:46:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m7/Assignment-1/Solution.java
+++ assignments/m7/Assignment-1/Solution.java
@@ -1,28 +1,25 @@
 import java.util.*;
-class InputValidator
-{
+class InputValidator {
 	String input;
-	InputValidator(String value){
+	InputValidator(String value) {
 		this.input  = value;
 	}
-	boolean validateData(){
+	boolean validateData() {
 		String str = this.input;
 		int len = str.length();
-		if(len>=6){
+		if (len >= 6) {
 			return true;
 		}
 		return false;
 	}
 }
-public class Solution
-{
-	public static void main(String args[])
-    {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
-    	System.out.println(i.validateData());
+public class Solution {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		String input = s.next();
+		InputValidator i = new InputValidator(input);
+		System.out.println(i.validateData());
 
-    }
+	}
 
 }
