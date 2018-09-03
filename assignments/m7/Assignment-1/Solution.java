648f7b60253c328a824593d7e26463fad7787875
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-09-03 18:50:00 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m7/Assignment-1/Solution.java
+++ assignments/m7/Assignment-1/Solution.java
@@ -3,41 +3,51 @@ import java.util.Scanner;
  * Class for input validator.
  */
 class InputValidator {
-	/**
-	 * { var_description }
-	 */
-	String input;
-	InputValidator(String value) {
-		/**
-		 * { item_description }
-		 */
-		this.input  = value;
-	}
-	boolean validateData() {
-		/**
-		 * { var_description }
-		 */
-		String str = this.input;
-		int len = str.length();
-		if (len >= 6) {
-			return true;
-		}
-		return false;
-	}
+    /**
+     * { var_description }.
+     */
+    String input;
+    /**
+     * Constructs the object.
+     *
+     * @param      value  The value
+     */
+    InputValidator(String value) {
+        /**
+         * { item_description }.
+         */
+        this.input  = value;
+    }
+    /**
+     * { returns true if the length is greater than or equal to six }.
+     *
+     * @return     {boolean value} }
+     */
+    boolean validateData() {
+        /**
+         * { var_description }
+         */
+        String str = this.input;
+        int len = str.length();
+        if (len >= 6) {
+            return true;
+        }
+        return false;
+    }
 }
 /**
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * { item_description }
-	 */
-	public static void main(String args[]) {
-		Scanner s = new Scanner(System.in);
-		String input = s.next();
-		InputValidator i = new InputValidator(input);
-		System.out.println(i.validateData());
+    /**
+     * { item_description }.
+     */
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
 
-	}
+    }
 
 }
