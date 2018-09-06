2976e990444e3af5770015f2bc89c0c2502313d1
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-09-06 15:08:59 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- assignments/m10/Assignment-1/List.java
+++ assignments/m10/Assignment-1/List.java
@@ -417,7 +417,7 @@ public class List {
                     if (t.length == 1) {
                         l.add(Integer.parseInt(tokens[1]));
                     } else {
-                        if (t.length > 1){
+                        if (t.length > 1) {
                          l.add(Integer.parseInt(t[0]), Integer.parseInt(t[1]));
                         }
                     }
@@ -430,7 +430,7 @@ public class List {
                 if (tokens.length == 2) {
                     String[] t1 = tokens[1].split(",");
                     int temp[] = new int[t1.length];
-                    for (int i = 0; i < temp.length; i++){
+                    for (int i = 0; i < temp.length; i++) {
                         temp[i] = Integer.parseInt(t1[i]);
                     }
                     l.addAll(temp);
@@ -465,3 +465,4 @@ public class List {
         }
     }
 }
+
