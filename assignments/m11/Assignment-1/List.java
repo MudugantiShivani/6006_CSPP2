dfe0ca0a8b173d3e9fe9f37d39e4e7759738d369
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-09-07 17:15:40 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- assignments/m11/Assignment-1/List.java
+++ assignments/m11/Assignment-1/List.java
@@ -427,7 +427,7 @@ public class List {
                     l.removeAll(a);
                 }
                 break;
-            case "subList": 
+            case "subList":
                 if (tokens.length != 2) {
                     break;
                 }
