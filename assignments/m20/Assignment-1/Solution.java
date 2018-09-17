a1ae7754be50124044323c96028fb02fd53547d0
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-09-17 18:01:51 +0530

    "check_style_score -> 13 of 13 passed. style score: 0/1 "

--- assignments/m20/Assignment-1/Solution.java
+++ assignments/m20/Assignment-1/Solution.java
@@ -199,11 +199,11 @@ public final class Solution {
     /**
      * {initialisng the int}.
      */
-    private static int Ten = 20;
+    private static int ten = 20;
     /**
      * { array for question text }.
      */
-    private static Question[] questiontext = new Question[Ten];
+    private static Question[] questiontext = new Question[ten];
     /**
      * { count variable }.
      */
